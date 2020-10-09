bb8393889fb88acf0c9dea7352951287f52bba20
SegFaulti4 <makar.popov01@mail.ru>
2020-10-09 21:39:44 +0400

    Added exceptions and Pipe class

--- hw-1/main.cpp
+++ hw-1/main.cpp
@@ -3,28 +3,64 @@
 #include <iostream>
 #include <sys/wait.h>
 
+class Pipe {
+private:
+    int fd[2] = {-1, -1};
+public:
+    explicit Pipe(int flag = 0) {
+        if (::pipe2(fd, flag) == -1) {
+            throw std::runtime_error("Pipe creation error");
+        }
+    }
+
+    ~Pipe() {
+        ::close(fd[0]);
+        ::close(fd[1]);
+    }
+
+    int rd() {
+        return fd[0];
+    }
+
+    int wr() {
+        return fd[1];
+    }
+
+    int close_rd() {
+        int res = ::close(fd[0]);
+        fd[0] = -1;
+        return res;
+    }
+
+    int close_wr() {
+        int res = ::close(fd[1]);
+        fd[1] = -1;
+        return res;
+    }
+
+    std::pair<int, int> close() {
+        return std::make_pair(close_rd(), close_wr());
+    }
+};
+
 class Process {
 private:
-    int pipe_rd_sub[2];
-    int pipe_wr_sub[2];
-    pid_t pid;
+    Pipe rd_sub, wr_sub;
+    pid_t pid = -1;
 
-public :
+public:
     explicit Process(const std::string& path) {
         pid = fork();
         if (pid != -1) {
-            pipe(pipe_rd_sub);
-            pipe(pipe_wr_sub);
-            int tmp_pipe[2];
-            pipe2(tmp_pipe, O_CLOEXEC);
+            Pipe tmp_pipe(O_CLOEXEC);
             if (pid == 0) {
                 execlp(path.data(), "child", nullptr);
                 exit(-1);
             }
             char tmp;
-            ::read(tmp_pipe[0], &tmp, sizeof(tmp));
-            int status;
-            std::cout << "Pid " << waitpid(pid, &status, WNOHANG) << std::endl;
+            if (::read(tmp_pipe.rd(), &tmp, sizeof(tmp)) == -1) {
+                throw std::runtime_error("Read error");
+            }
         }
     }
 
@@ -41,23 +77,22 @@ public :
     void pid_update() {
         int status;
         waitpid(pid, &status, WNOHANG);
-        if (WIFEXITED(status) || WIFEXITED(status))
+        if (WIFEXITED(status) || WIFEXITED(status)) {
             pid = -1;
+        }
     }
 
     void closeStdin() {
         pid_update();
         if (!pid || pid != -1) {
-            ::close(pipe_wr_sub[0]);
-            ::close(pipe_wr_sub[1]);
+            rd_sub.close();
         }
     }
 
     void closeStdout() {
         pid_update();
         if (!pid || pid != -1) {
-            ::close(pipe_wr_sub[1]);
-            ::close(pipe_wr_sub[0]);
+            wr_sub.close();
         }
     }
 
@@ -72,32 +107,41 @@ public :
     }
 
     size_t write(const void *data, size_t len) {
+        if (data == nullptr) {
+            return 0;
+        }
         pid_update();
         if (!pid) {
-            return ::write(pipe_rd_sub[1], data, len);
+            return ::write(rd_sub.wr(), data, len);
         } else if (pid != -1) {
-            return ::write(pipe_wr_sub[1], data, len);
+            return ::write(wr_sub.rd(), data, len);
         }
         return 0;
     }
 
     size_t read(void *data, size_t len) {
+        if (data == nullptr) {
+            return 0;
+        }
         pid_update();
         if (!pid) {
-            return ::read(pipe_wr_sub[0], data, len);
+            return ::read(wr_sub.rd(), data, len);
         } else if (pid != -1) {
-            return ::read(pipe_rd_sub[0], data, len);
+            return ::read(rd_sub.rd(), data, len);
         }
         return 0;
     }
 
     void writeExact(const void *data, size_t len) {
+        if (data == nullptr) {
+            return;
+        }
         pid_update();
         size_t cur = 0;
         size_t res;
         if (!pid) {
             while (cur < len) {
-                res = ::write(pipe_rd_sub[1],
+                res = ::write(rd_sub.wr(),
                                (const unsigned char *)data + cur, len - cur);
                 if (res <= 0) {
                     return;
@@ -106,7 +150,7 @@ public :
             }
         } else if (pid != -1) {
             while (cur < len) {
-                res = ::write(pipe_wr_sub[1],
+                res = ::write(wr_sub.wr(),
                                (const unsigned char *)data + cur, len - cur);
                 if (res <= 0) {
                     return;
@@ -117,12 +161,15 @@ public :
     }
 
     void readExact(void *data, size_t len) {
+        if (data == nullptr) {
+            return;
+        }
         pid_update();
         size_t cur = 0;
         size_t res;
         if (!pid) {
             while (cur < len) {
-                res = ::read(pipe_wr_sub[0],
+                res = ::read(wr_sub.rd(),
                                (unsigned char *)data + cur, len - cur);
                 if (res <= 0) {
                     return;
@@ -131,7 +178,7 @@ public :
             }
         } else if (pid != -1) {
             while (cur < len) {
-                res = ::read(pipe_rd_sub[0],
+                res = ::read(rd_sub.rd(),
                                (unsigned char *)data + cur, len - cur);
                 if (res <= 0) {
                     return;
